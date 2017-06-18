137c78d4ddf6bd5f2b8de0f6d3b9be19ca55426d
heineiuo <heineiuo@gmail.com>
2017-06-18 10:06:00 +0800

    Publish 0.5.0

--- src/index.js
+++ src/index.js
@@ -18,7 +18,40 @@ const createDispatch = (ctx) => (actionCreator) => {
   return actionCreator(createDispatch(ctx), () => ctx)
 }
 
+const seashellActionMiddleware = async ctx => {
+  const sendError = (error) => {
+    ctx.response.body = {error}
+    ctx.response.end()
+  };
+  const paths = ctx.request.headers.originUrl.split('/').filter(item => item !== '')
+  const walk = async (currentActionCreators) => {
+    if (paths.length === 0) return sendError('NOT_FOUND');
+    const currentAction = currentActionCreators[paths.shift()];
+    if (typeof currentAction === 'undefined') return sendError('NOT_FOUND');
+    if (typeof currentAction === 'object' ) {
+      return process.nextTick(() => walk(currentAction))
+    }
+    if (typeof currentAction === 'function') {
+      if (paths.length > 0) return sendError('NOT_FOUND');
+      const dispatch = createDispatch(ctx);
+      const actionType = dispatch(currentAction(ctx.request.body));
+      if (actionType instanceof Promise) {
+        try {
+          ctx.response.body = await actionType;
+          ctx.response.end()
+        } catch(e){
+          sendError(e);
+        }
+      }
+      return null;
+    }
+    return sendError('ERROR_TYPE_OF_ACTION')
+  }
+  process.nextTick(() => walk(allActionCreators))
+}
+
 export {
   createDispatch,
-  bindActionCreators
+  bindActionCreators,
+  seashellActionMiddleware
 }
