5429aee6c5f93627c0ca71629be1d304db859ec1
heineiuo <heineiuo@gmail.com>
2017-07-07 14:40:23 +0800

    Publish action-creator@0.6.3

--- src/index.js
+++ src/index.js
@@ -27,32 +27,52 @@ const createDispatch = (ctx) => (actionCreator) => {
  * @param {*} onSuccess 
  * @param {*} onError 
  */
-const pathsToActions = async (ctx, paths, currentActionCreators, onSuccess, onError) => {
-  if (paths.length === 0) return onError(new Error('NOT_FOUND'));
-  const currentAction = currentActionCreators[paths.shift()];
-  if (typeof currentAction === 'undefined') return onError(new Error('NOT_FOUND'));
-  if (typeof currentAction === 'object' ) {
-    return process.nextTick(() => pathsToActions(
+const pathsToActions = (ctx, paths, currentActionCreators, onSuccess, onError) => {
+  const handleObject = (currentAction, callback=null) => {
+    if (currentAction instanceof Array) return handleList(currentAction.slice())
+    if (paths.length === 0) return onError(new Error('NOT_FOUND'));
+    const nextAction = currentAction[paths.shift()]
+    return pathsToActions(
       ctx,
       paths,
-      currentAction,
+      nextAction,
       onSuccess,
       onError
-    ))
+    )
   }
-  if (typeof currentAction === 'function') {
-    if (paths.length > 0) return onError(new Error('NOT_FOUND'));
+
+  const handleList = (list) => {
+    if (list.length === 0) return onError(new Error('NOT_FOUND'))
+    const walkToSolveList = (list, prevResult) => {
+      if (list.length === 0) return onSuccess(prevResult)
+      const current = list.shift()
+      if (typeof current === 'function') return handleFunction(current, (result) => walkToSolveList(list, result))
+      if (typeof current === 'object') return handleObject(current)
+      return onError(new Error('ERROR_TYPE_OF_ACTION'))
+    }
+    return walkToSolveList(list)
+  }
+
+  const handleFunction = async (currentAction, callback=null) => {
+    if (!callback && paths.length > 0) return onError(new Error('NOT_FOUND'));
     const dispatch = createDispatch(ctx);
     const actionType = dispatch(currentAction(ctx.request.body));
     if (actionType instanceof Promise) {
       try {
-        onSuccess(await actionType)
+        const result = await actionType
+        if (!callback) return onSuccess(result)
+        callback(result)
       } catch(e){
         onError(e);
       }
     }
     return null;
   }
+
+  if (typeof currentActionCreators === 'object') return handleObject(currentActionCreators);
+  if (typeof currentActionCreators === 'function') return handleFunction(currentActionCreators)
+  if (typeof currentActionCreators === 'undefined') return onError(new Error('NOT_FOUND'));
+  
   return onError(new Error('ERROR_TYPE_OF_ACTION'))
 }
 
